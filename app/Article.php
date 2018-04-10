edd4936bb91d2b7a3966db738e4691a4df393952
baselrabia <baselrabia@gmail.com>
2018-04-10 04:55:59 +0200

    first push

---
+++ app/Article.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Article extends Model
+{
+    //
+    public $table="articles";
+    
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+
+}
+
+/*
+class Article extends Model
+{
+    public $table="articles";
+
+    public function user()
+    {
+    	return $this->belongsTo('App/User');
+    }
+    public function comments()
+    {
+    	return $this->hasMany('App/Comment');
+    }
+
+
+}
+
+
+
